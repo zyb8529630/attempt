5ffa61bcbd1e8f1c49599eed15a01101da2ea03f
AB063825 <AB063825@BJ-AB051964-MI2.AB.COM>
2019-06-05 12:50:30 +0800

    添加业务异常类

---
+++ attempt-common/src/main/java/com/attempt/core/exception/BusinessException.java
@@ -0,0 +1,59 @@
+package com.attempt.core.exception;
+
+import org.springframework.util.Assert;
+
+/**
+ * 抽象的业务异常对象.
+ * @author zhouyinbin
+ * @date 2019年6月5日 下午12:46:01
+ *
+ */
+public class BusinessException  extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * 业务错误码. 默认0000
+     */
+    private String errorCode = "00000";
+
+    /**
+     * 构造方法.
+     *
+     * @param message 异常消息
+     * @param cause   关联异常
+     */
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * 构造方法.
+     *
+     * @param cause 关联异常
+     */
+    public BusinessException(Throwable cause) {
+        super(cause.getMessage(), cause);
+    }
+
+    /**
+     * 构造方法.
+     *
+     * @param errorCode 错误码
+     * @param message   异常消息
+     */
+    public BusinessException(String errorCode, String message) {
+        super(message);
+        Assert.notNull(errorCode);
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * 获取错误码.
+     *
+     * @return 错误码
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
